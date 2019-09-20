3135b5fb282203af2271a21d512f2507faa1b7e1
David L. Day <dday376@gmail.com>
2019-09-20 07:17:09 -0400

    roughed out calls

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { isNullOrUndefined } from 'util';
+import * as rp from 'request-promise-native';
+import { resolve } from 'url';
 
 let diagnosticCollection: vscode.DiagnosticCollection;
 let diagnosticMap: Map<string, vscode.Diagnostic[]>;
@@ -11,8 +13,15 @@ let diagnosticMap: Map<string, vscode.Diagnostic[]>;
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+  let ltDocumentLanguage = [ "markdown", "plaintext" ];
   diagnosticCollection = vscode.languages.createDiagnosticCollection("LanguageTool Linter");
   diagnosticMap = new Map();
+
+  function isWriteGoodLanguage(languageId) {
+    // let wgLanguages = vscode.workspace.getConfiguration('write-good').get('languages');
+    // return (ltDocumentLanguage.indexOf(languageId) > -1 || ltDocumentLanguage === '*');
+    return (ltDocumentLanguage.indexOf(languageId) > -1);
+  }
 
   context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
     if (isWriteGoodLanguage(event.document.languageId)) {
@@ -20,22 +29,32 @@ export function activate(context: vscode.ExtensionContext) {
     }
   }));
 
+  context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(event => {
+    if (isWriteGoodLanguage(event.languageId)) {
+        doLint(event);
+    }
+  }));
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "languagetool-linter" is now active!');
+  context.subscriptions.push(vscode.workspace.onDidCloseTextDocument(event => {
+    if (diagnosticMap.has(event.uri.toString())) {
+        diagnosticMap.delete(event.uri.toString());
+    }
+    resetDiagnostics();
+  }));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	console.log('LanguageTool Linter activated!');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	// // The command has been defined in the package.json file
+	// // Now provide the implementation of the command with registerCommand
+	// // The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-	context.subscriptions.push(disposable);
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World!');
+	// });
+
+	// context.subscriptions.push(disposable);
 }
 
 interface Suggestion {
@@ -53,11 +72,41 @@ function resetDiagnostics() {
 }
 
 function doLint(document: vscode.TextDocument) {
+
   let ltConfig = vscode.workspace.getConfiguration('languagetool-linter');
   // if (isNullOrUndefined(ltConfig)) {
   //   ltConfig = {};
   // }
   let diagnostics: vscode.Diagnostic[] = [];
+
+  let editorContent = document.getText();
+
+  let post_data_dict = {
+    "language": "auto",
+    "text": editorContent,
+    "preferredVariants": ltConfig.get('languageTool.preferredVariants'),
+    "disabledCategories": ltConfig.get('languageTool.disabledCategories'),
+    "disabledRules": ltConfig.get('languageTool.disabledRules'),
+    "motherTongue": ltConfig.get("languageTool.motherTongue")
+  };
+
+  let options = {
+    method: 'POST',
+    uri: "https://languagetool.org/api/v2/check",
+    form: post_data_dict,
+    json: true
+  };
+
+  rp(options)
+    .then( function (data) {
+      resolve(data,data);
+    })
+    .catch( function (err) {
+      console.log(err);
+      resolve(err, err);
+    });
+
+
   let lines = document.getText().split(/\r?\n/g);
   lines.forEach((line, lineCount) => {
       let suggestions : Suggestion[] = WriteGood(line, ltConfig);
@@ -73,4 +122,6 @@ function doLint(document: vscode.TextDocument) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  console.log("LanguageTool Linter deactivated...");
+}
