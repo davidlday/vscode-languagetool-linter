d4836ccf3135976bbf8de802e2b457685084cf08
David L. Day <dday376@gmail.com>
2019-10-18 20:41:40 -0400

    added smart dashes

---
+++ src/typeFormatters/dashesFormatter.ts
@@ -0,0 +1,34 @@
+// import { CancellationToken, FormattingOptions, OnTypeFormattingEditProvider, Position, TextDocument, TextEdit, TextLine, Range } from 'vscode';
+import * as vscode from 'vscode';
+
+export class DashesFormattingProvider implements vscode.OnTypeFormattingEditProvider {
+  private readonly emDash: string = '—';
+  private readonly enDash: string = '–';
+  private readonly hyphen: string = '-';
+  static readonly triggers: string[] = ['-'];
+
+  public provideOnTypeFormattingEdits(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    ch: string,
+    options: vscode.FormattingOptions,
+    cancellationToken: vscode.CancellationToken): vscode.TextEdit[] {
+
+    const line: vscode.TextLine = document.lineAt(position.line);
+    let range: vscode.Range = new vscode.Range(position.line, position.character - 2, position.line, position.character);
+    const prevCh: string = (position.character > 0) ? line.text.charAt(position.character - 2) : " ";
+    const prevPrevCh: string = (position.character > 1) ? line.text.charAt(position.character - 3) : " ";
+
+    if (prevCh === this.enDash) {
+      return [new vscode.TextEdit(range, this.emDash)];
+    } else if (prevCh === this.hyphen) {
+      if (prevPrevCh === this.hyphen) {
+        let range: vscode.Range = new vscode.Range(position.line, position.character - 3, position.line, position.character);
+        return [new vscode.TextEdit(range, this.emDash)];
+      } else {
+        return [new vscode.TextEdit(range, this.enDash)];
+      }
+    }
+    return [];
+  }
+}
