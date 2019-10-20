79c11eaa12c29cd2b27f50e31f59ab2e4731783f
David L. Day <dday376@gmail.com>
2019-10-20 13:22:03 -0400

    corrected character offsets

--- src/typeFormatters/quotesFormatter.ts
+++ src/typeFormatters/quotesFormatter.ts
@@ -23,8 +23,10 @@ export class QuotesFormattingProvider implements vscode.OnTypeFormattingEditProv
 
     const line: vscode.TextLine = document.lineAt(position.line);
     const chRange: vscode.Range = new vscode.Range(position.line, position.character - 1, position.line, position.character);
-    const prevCh: string = (position.character > 0) ? line.text.charAt(position.character - 2) : " ";
+    const prevCh: string = (position.character > 1) ? line.text.charAt(position.character - 2) : " ";
     const nextCh: string = (position.character < line.text.length) ? line.text.charAt(line.text.length + 1) : " ";
+
+    console.log("'" + prevCh + ch + nextCh + "'");
 
     if (this.config.isAutoFormatEnabled()) {
       switch (ch) {
