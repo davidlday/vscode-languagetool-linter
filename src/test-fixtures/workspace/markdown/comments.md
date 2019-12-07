186a5d01c4361ec7e37d9bc8599a4efcbc1d2310
David L. Day <dday376@gmail.com>
2019-12-07 15:44:53 -0500

    added markdown comments test case

---
+++ src/test-fixtures/workspace/markdown/comments.md
@@ -0,0 +1,10 @@
+# Comments Test Case
+
+Smart formatting should ignore comments.
+
+<!--
+Like this.
+Unfortunately the linter will also ignore comments.
+-->
+
+But that’s okay for now.
--- src/test/suite/linter.markdown.test.ts
+++ src/test/suite/linter.markdown.test.ts
@@ -22,6 +22,14 @@ suite('Linter Markdown Test Suite', () => {
     assert.deepStrictEqual(actual, expected);
   });
 
+  test('Linter should return annotated text for Markdown with Comments', () => {
+    const expected = JSON.parse(fs.readFileSync(path.resolve(__dirname, testWorkspace + "/markdown/comments.json"), "utf8"));
+    const text = fs.readFileSync(path.resolve(__dirname, testWorkspace + "/markdown/comments.md"), "utf8");
+    const actual = linter.buildAnnotatedMarkdown(text);
+    // fs.writeFileSync(path.resolve(__dirname, testWorkspace + "/markdown/comments.json"), JSON.stringify(actual), "utf8");
+    assert.deepStrictEqual(actual, expected);
+  });
+
   test('Linter should return annotated text for Markdown with Front Matter (YAML)', () => {
     const expected = JSON.parse(fs.readFileSync(path.resolve(__dirname, testWorkspace + "/markdown/front-matter.json"), "utf8"));
     const text = fs.readFileSync(path.resolve(__dirname, testWorkspace + "/markdown/front-matter.md"), "utf8");
