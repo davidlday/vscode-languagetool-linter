3a3b218352837c311b9683963164f0fd6bf91db7
David L. Day <dday376@gmail.com>
2019-12-29 15:57:10 -0500

    use double quotes

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,27 @@
 //@ts-check
 
-'use strict';
+"use strict";
 
-const path = require('path');
-const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
+const path = require("path");
+const LicenseWebpackPlugin = require("license-webpack-plugin").LicenseWebpackPlugin;
 
-/**@type {import('webpack').Configuration}*/
+/**@type {import("webpack").Configuration}*/
 const config = {
-    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-    output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'extension.js',
+    entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: { // the bundle is stored in the "dist" folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, "dist"),
+        filename: "extension.js",
         libraryTarget: "commonjs2",
         devtoolModuleFilenameTemplate: "../[resource-path]",
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     externals: {
         vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     },
     resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-        extensions: ['.ts', '.js']
+        extensions: [".ts", ".js"]
     },
     plugins: [
       new LicenseWebpackPlugin({
@@ -31,9 +31,9 @@ const config = {
         },
         perChunkOutput: true,
         addBanner: true,
-        preferredLicenseTypes: ['MIT', 'ISC'],
+        preferredLicenseTypes: ["MIT", "ISC"],
         handleMissingLicenseText: (packageName, licenseType) => {
-          console.log('Cannot find license for ' + packageName + " (" + licenseType + ")");
+          console.log("Cannot find license for " + packageName + " (" + licenseType + ")");
           return licenseType;
         }
       })
@@ -43,7 +43,7 @@ const config = {
             test: /\.ts$/,
             exclude: /node_modules/,
             use: [{
-                loader: 'ts-loader',
+                loader: "ts-loader",
                 options: {
                     compilerOptions: {
                         "module": "es6" // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
