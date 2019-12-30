7f418fb9808a1bef265c2c7cde4d0e9e9ffc8b43
David L. Day <dday376@gmail.com>
2019-12-29 21:15:56 -0500

    codacy cleanup

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,10 +7,9 @@ const LicenseWebpackPlugin = require("license-webpack-plugin").LicenseWebpackPlu
 
 /**@type {import("webpack").Configuration}*/
 const config = {
-    target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-
-    entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-    output: { // the bundle is stored in the "dist" folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    target: "node",
+    entry: "./src/extension.ts",
+    output: {
         path: path.resolve(__dirname, "dist"),
         filename: "extension.js",
         libraryTarget: "commonjs2",
@@ -18,16 +17,16 @@ const config = {
     },
     devtool: "source-map",
     externals: {
-        vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        vscode: "commonjs vscode",
     },
-    resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    resolve: {
         extensions: [".ts", ".js"]
     },
     plugins: [
       new LicenseWebpackPlugin({
         stats: {
+          errors: true,
           warnings: false,
-          errors: true
         },
         perChunkOutput: true,
         addBanner: true,
@@ -46,7 +45,7 @@ const config = {
                 loader: "ts-loader",
                 options: {
                     compilerOptions: {
-                        "module": "es6" // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
+                        "module": "es6"
                     }
                 }
             }]
