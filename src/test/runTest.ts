781aed1fa1db7cc25cbe6db50616f90d739d4d29
David L. Day <dday376@gmail.com>
2019-12-29 21:12:13 -0500

    codacy cleanup

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,21 +4,21 @@ import { runTests } from 'vscode-test';
 
 async function main() {
 	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
+    // The path to test runner
+    // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
     const testWorkspace = path.resolve(__dirname, '../../test/test-fixtures/workspace');
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace] });
+    // Download VS Code, unzip it and run the integration test
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace] });
 	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
+    console.error('Failed to run tests');
+    process.exit(1);
 	}
 }
 
