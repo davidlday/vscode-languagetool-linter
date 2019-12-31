c239e476ad50f197d1dbdee6965048ddbf5b2a11
David L. Day <dday376@gmail.com>
2019-12-30 23:01:08 -0500

    code cleanup

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,39 +1,41 @@
-import * as assert from 'assert';
-import { before } from 'mocha';
-import * as vscode from 'vscode';
+import * as assert from "assert";
+// tslint:disable-next-line: no-implicit-dependencies
+import { before } from "mocha";
+import * as vscode from "vscode";
 
 // import * as languagetooLinter from '../';
 
-suite('Extension Test Suite', () => {
-	before(() => {
-		vscode.window.showInformationMessage('Start all tests.');
-	});
+suite("Extension Test Suite", () => {
 
-	test('Extension should be present', () => {
-		assert.ok(vscode.extensions.getExtension('davidlday.languagetool-linter'));
-	});
+  before(() => {
+    vscode.window.showInformationMessage("Start all tests.");
+  });
 
-  test('Extension should activate', function () {
+  test("Extension should be present", () => {
+    assert.ok(vscode.extensions.getExtension("davidlday.languagetool-linter"));
+  });
+
+  test("Extension should activate", function() {
     this.timeout(60000);
-    let ext: any = vscode.extensions.getExtension('davidlday.languagetool-linter');
+    const ext: any = vscode.extensions.getExtension("davidlday.languagetool-linter");
     return ext.activate().then((api: any) => {
       assert.ok(true);
     });
-	});
+  });
 
-  test('Extension should register all commands', () => {
+  test("Extension should register all commands", () => {
     return vscode.commands.getCommands(true).then((commands) => {
       const EXPECTED_COMMANDS: string[] = ["languagetoolLinter.lintCurrentDocument",
         "languagetoolLinter.smartFormatDocument",
         "languagetoolLinter.ignoreWordGlobally",
         "languagetoolLinter.ignoreWordInWorkspace",
         "languagetoolLinter.removeGloballyIgnoredWord",
-        "languagetoolLinter.removeWorkspaceIgnoredWord"
+        "languagetoolLinter.removeWorkspaceIgnoredWord",
       ];
       const FOUND_COMMANDS = commands.filter((value) => {
-				return EXPECTED_COMMANDS.indexOf(value)>=0 || value.startsWith('languagetoolLinter.');
+        return EXPECTED_COMMANDS.indexOf(value) >= 0 || value.startsWith("languagetoolLinter.");
       });
-      assert.equal(FOUND_COMMANDS.length , EXPECTED_COMMANDS.length, 'Either not all commands are registered or new commands have not been added to this test.');
+      assert.equal(FOUND_COMMANDS.length , EXPECTED_COMMANDS.length, "Either not all commands are registered or new commands have not been added to this test.");
     });
   });
 
