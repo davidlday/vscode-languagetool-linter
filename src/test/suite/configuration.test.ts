d4c08884b0fadd1dde00d847348b8c40527ddd7e
David L. Day <dday376@gmail.com>
2019-12-30 23:01:01 -0500

    code cleanup

--- src/test/suite/configuration.test.ts
+++ src/test/suite/configuration.test.ts
@@ -1,17 +1,17 @@
-import * as assert from 'assert';
-import * as vscode from 'vscode';
-import { ConfigurationManager } from '../../common/configuration-manager';
-import * as constants from '../../common/constants';
+import * as assert from "assert";
+import * as vscode from "vscode";
+import { ConfigurationManager } from "../../common/configuration-manager";
+import * as constants from "../../common/constants";
 
-suite('ConfigurationManager Test Suite', () => {
+suite("ConfigurationManager Test Suite", () => {
 
-	test('ConfigurationManager should load configuration', () => {
-		assert.ok(vscode.workspace.getConfiguration(constants.LT_CONFIGURATION_ROOT));
-	});
+  test("ConfigurationManager should load configuration", () => {
+    assert.ok(vscode.workspace.getConfiguration(constants.LT_CONFIGURATION_ROOT));
+  });
 
-  test('ConfigurationManager should return the default service URL', () => {
+  test("ConfigurationManager should return the default service URL", () => {
     const EXPECTED_DEFAULT_URL = "http://localhost:8081/v2/check";
-    let config: ConfigurationManager = new ConfigurationManager();
+    const config: ConfigurationManager = new ConfigurationManager();
     assert.equal(EXPECTED_DEFAULT_URL, config.getUrl(), "Default URL is not correct");
   });
 
