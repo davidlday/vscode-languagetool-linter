ebc290e562a43b9f163053601638344cbdacbc0b
David L. Day <dday376@gmail.com>
2020-01-25 12:02:37 -0500

    refactored constants

--- src/test/suite/configuration.test.ts
+++ src/test/suite/configuration.test.ts
@@ -1,18 +1,18 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
 import { ConfigurationManager } from "../../common/configuration-manager";
-import * as constants from "../../common/constants";
+// import * as constants from "../../common/constants";
 
 suite("ConfigurationManager Test Suite", () => {
 
   test("ConfigurationManager should load configuration", () => {
-    assert.ok(vscode.workspace.getConfiguration(constants.LT_CONFIGURATION_ROOT));
+    assert.ok(vscode.workspace.getConfiguration(ConfigurationManager.CONFIGURATION_ROOT));
   });
 
   test("ConfigurationManager should return the default service URL", () => {
     const EXPECTED_DEFAULT_URL = "http://localhost:8081/v2/check";
     const config: ConfigurationManager = new ConfigurationManager();
-    assert.equal(EXPECTED_DEFAULT_URL, config.getUrl(), "Default URL is not correct");
+    assert.equal(EXPECTED_DEFAULT_URL, config.getUrl());
   });
 
 });
