c3e2cfb40efab76b68dc540ba30998bbb4f9960e
David L. Day <dday376@gmail.com>
2020-06-21 10:51:40 -0400

    removed all plaintext constants

--- src/configuration/constants.ts
+++ src/configuration/constants.ts
@@ -27,7 +27,6 @@ export const EXTENSION_DIAGNOSTIC_SOURCE = "LanguageTool";
 // Programming Language IDs
 export const LANGUAGE_ID_MARKDOWN = "markdown";
 export const LANGUAGE_ID_HTML = "html";
-export const LANGUAGE_ID_PLAINTEXT = "plaintext";
 
 // File Scheme
 export const SCHEME_FILE = "file";
@@ -50,21 +49,12 @@ export const SELECTOR_HTML_UNTITLED: DocumentSelector = {
   language: LANGUAGE_ID_HTML,
   scheme: SCHEME_UNTITLED,
 };
-// export const SELECTOR_PLAINTEXT_FILE: DocumentSelector = {
-//   language: LANGUAGE_ID_PLAINTEXT,
-//   scheme: SCHEME_FILE,
-// };
-// export const SELECTOR_PLAINTEXT_UNTITLED: DocumentSelector = {
-//   language: LANGUAGE_ID_PLAINTEXT,
-//   scheme: SCHEME_UNTITLED,
-// };
+
 export const DOCUMENT_SELECTORS: DocumentSelector[] = [
   SELECTOR_MARKDOWN_FILE,
   SELECTOR_MARKDOWN_UNTITLED,
   SELECTOR_HTML_FILE,
   SELECTOR_HTML_UNTITLED,
-  // SELECTOR_PLAINTEXT_FILE,
-  // SELECTOR_PLAINTEXT_UNTITLED,
 ];
 
 // Configuration Strings
@@ -77,7 +67,6 @@ export const CONFIGURATION_IGNORED_WORD_HINT = "languageTool.ignoredWordHint";
 export const CONFIGURATION_DOCUMENT_LANGUAGE_IDS: string[] = [
   LANGUAGE_ID_MARKDOWN,
   LANGUAGE_ID_HTML,
-  // LANGUAGE_ID_PLAINTEXT,
 ];
 export const CONFIGURATION_PLAIN_TEXT_ENABLED = "plainText.enabled";
 export const CONFIGURATION_PLAIN_TEXT_IDS = "plainText.languageIds";
