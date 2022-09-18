14b2e428a0a9b546b1701258571c8c3a64145f80
David L. Day <dday376@gmail.com>
2022-09-18 07:48:46 -0400

    basic commands

--- src/PodmanService.ts
+++ src/PodmanService.ts
@@ -13,36 +13,172 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-import * as portfinder from "portfinder";
-import {
-  commands,
-  ConfigurationChangeEvent,
-  ConfigurationTarget,
-  DiagnosticSeverity,
-  Disposable,
-  DocumentSelector,
-  TextDocument,
-  Uri,
-  window,
-  workspace,
-  WorkspaceConfiguration,
-} from "vscode";
+
+import execa from "execa";
+import { Disposable, workspace, WorkspaceConfiguration } from "vscode";
 import * as Constants from "./Constants";
 
+export interface ContainerInfo {
+  Id: string;
+  Created: string;
+  Path: string;
+  Args: string[];
+  State: {
+    Status: string;
+    Running: boolean;
+    Paused: boolean;
+    Restarting: boolean;
+    OOMKilled: boolean;
+    Dead: boolean;
+    Pid: number;
+    ExitCode: number;
+    Error: string;
+    StartedAt: string;
+    FinishedAt: string;
+    Health: {
+      Status: string;
+      FailingStreak: number;
+      Log: {
+        Start: string;
+        End: string;
+        ExitCode: number;
+        Output: string;
+      }[];
+    };
+  };
+  Image: string;
+  ImageName: string;
+  Name: string;
+  HostConfig: {
+    PortBindings: {
+      "8010/tcp": {
+        HostIp: string;
+        HostPort: string;
+      }[];
+    };
+  };
+}
+
+export interface MachineInfo {
+  Host: {
+    Arch: string;
+    CurrentMachine: string;
+    DefaultMachine: string;
+    EventsDir: string;
+    MachineConfigDir: string;
+    MachineImageDir: string;
+    MachineState: string;
+    NumberOfMachines: number;
+    OS: string;
+    VMType: string;
+  };
+  Version: {
+    APIVersion: string;
+    Version: string;
+    GoVersion: string;
+    GitCommit: string;
+    BuiltTime: string;
+    Built: number;
+    OsArch: string;
+    Os: string;
+  };
+}
+
 export class PodmanService implements Disposable {
-  private managedPort: number | undefined;
+  private config: WorkspaceConfiguration;
+  private containerName: string;
+  private imageName: string;
 
   // Constructor
-  constructor() {
+  constructor(imageName: string, containerName: string) {
     this.config = workspace.getConfiguration(Constants.CONFIGURATION_ROOT);
-    this.serviceUrl = this.findServiceUrl(this.getServiceType());
-    this.startManagedService();
-    this.serviceParameters = this.buildServiceParameters();
+    this.containerName = containerName;
+    this.imageName = imageName;
+  }
+
+  // Private instance methods
+  private machineInfo(): MachineInfo {
+    const result = execa.sync("podman", [
+      "machine",
+      "info",
+      "--format",
+      "json",
+    ]);
+    return JSON.parse(result.stdout);
+  }
+
+  private inspectContainer(): ContainerInfo[] {
+    const result = execa.sync("podman", [
+      "inspect",
+      this.containerName,
+      "--format",
+      "json",
+    ]);
+    return JSON.parse(result.stdout);
+  }
+
+  private startContainer(): void {
+    execa.sync("podman", ["start", this.containerName]);
+  }
+
+  private stopContainer(): void {
+    execa.sync("podman", ["stop", this.containerName]);
+  }
+
+  private startMachine(): void {
+    execa.sync("podman", ["machine", "start"]);
   }
 
   // Public instance methods
+  public run(): void {
+    execa.sync("podman", [
+      "run",
+      "-d",
+      "-p",
+      ":8010",
+      "--name",
+      this.containerName,
+      this.imageName,
+    ]);
+
+    // Wait for the container to start
+    while (!this.isRunning()) {
+      // Do nothing
+    }
+  }
 
   public dispose(): void {
-    this.stopManagedService();
+    this.stop();
+  }
+
+  public start(): void {
+    if (this.machineInfo().Host.MachineState !== "Running") {
+      this.startMachine();
+    }
+    this.startContainer();
+  }
+
+  public stop(): void {
+    this.stopContainer();
+  }
+
+  public isRunning(): boolean {
+    return this.machineInfo().Host.MachineState === "Running"
+      ? this.inspectContainer()[0].State.Running
+      : false;
+  }
+
+  public rename(newName: string): void {
+    execa.sync("podman", ["rename", this.containerName, newName]);
+    this.containerName = newName;
+  }
+
+  public port(): string {
+    const containerInfo: ContainerInfo[] = this.inspectContainer();
+    return containerInfo[0].HostConfig.PortBindings["8010/tcp"][0].HostPort;
+  }
+
+  public name(): string {
+    return this.containerName;
   }
 }
