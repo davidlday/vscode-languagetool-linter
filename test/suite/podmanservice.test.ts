8b48236afc00fdbe1ddadec35c5a11eec7c76ee7
David L. Day <dday376@gmail.com>
2022-09-22 19:52:16 -0400

    added more podman tests

--- test/suite/podmanservice.test.ts
+++ test/suite/podmanservice.test.ts
@@ -3,21 +3,41 @@ import * as vscode from "vscode";
 import * as Constants from "../../src/Constants";
 import { PodmanService } from "../../src/PodmanService";
 
-suite("PodmanService Test Suite", () => {
-  test("PodmanService should instantiate", () => {
-    const podman = new PodmanService(
-      "docker.io/erikvl87/languagetool:5.8",
-      "vscode-languagetool-linter-test",
-    );
+suite("PodmanService Test Suite", function () {
+  const containerName = "languagetool";
+  const imageName = "docker.io/erikvl87/languagetool:5.8";
+
+  const podman = new PodmanService(imageName, containerName);
+
+  test("PodmanService should instantiate", function () {
     assert.ok(podman);
   });
 
-  test("PodmanService should create a new container", () => {
-    const podman = new PodmanService(
-      "docker.io/erikvl87/languagetool:5.8",
-      "vscode-languagetool-linter-test",
-    );
-    podman.run();
-    assert.ok(podman.port());
+  test("PodmanService should NOT be running", function () {
+    const running = podman.isContainerRunning();
+    assert.strictEqual(running, false);
+  });
+
+  test("PodmanService should create a new container", function () {
+    this.timeout(10000);
+    podman.runContainer();
+    assert.ok(podman.getPort());
+  });
+
+  test("PodmanService should be running", function () {
+    const running = podman.isContainerRunning();
+    assert.strictEqual(running, true);
+  });
+
+  test("PodmanService should stop", function () {
+    this.timeout(12000);
+    podman.stop();
+    const running = podman.isContainerRunning();
+    assert.strictEqual(running, false);
+  });
+
+  test("PodmanService should NOT be running", function () {
+    const running = podman.isContainerRunning();
+    assert.strictEqual(running, false);
   });
 });
