97fc488af7d9811a4d03a016a5d1c3bb9c3b832a
David L. Day <dday376@gmail.com>
2022-09-24 09:07:58 -0400

    added service state

--- src/services/AbstractService.ts
+++ src/services/AbstractService.ts
@@ -27,6 +27,7 @@ import * as Constants from "../Constants";
 export abstract class AbstractService
   implements Disposable, ILanguageToolService
 {
+  protected _state = Constants.ServiceStates.STOPPED;
   protected _workspaceConfig: WorkspaceConfiguration;
   protected _ltUrl: string | undefined;
 
@@ -38,22 +39,33 @@ export abstract class AbstractService
     return this._ltUrl;
   }
 
+  public getState(): string {
+    return this._state;
+  }
+
   public reloadConfiguration(
     event: ConfigurationChangeEvent,
     workspaceConfig: WorkspaceConfiguration,
   ): void {
     if (event.affectsConfiguration(Constants.CONFIGURATION_ROOT)) {
+      this.stop();
+      while (this._state !== Constants.ServiceStates.STOPPED) {
+        // wait for stop to complete
+      }
       this._workspaceConfig = workspaceConfig;
+      this.start();
+      while (this._state !== Constants.ServiceStates.RUNNING) {
+        // wait for start to complete
+      }
     }
   }
 
   public invokeLanguageTool(
-    document: TextDocument,
     ltPostDataDict: Record<string, string>,
   ): Promise<ILanguageToolResponse> {
     return new Promise((resolve, reject) => {
       const url = this.getURL();
-      if (url) {
+      if (this._state === Constants.ServiceStates.RUNNING && url) {
         const formBody = Object.keys(ltPostDataDict)
           .map(
             (key: string) =>
@@ -75,21 +87,49 @@ export abstract class AbstractService
           .then((res) => res.json())
           .then((json) => resolve(json as ILanguageToolResponse))
           .catch((err) => {
-            reject(err.message);
+            reject(err);
           });
+      } else if (this._state !== Constants.ServiceStates.RUNNING) {
+        switch (this._state) {
+          case Constants.ServiceStates.STOPPED:
+            reject(new Error("LanguageTool called on stopped service."));
+            break;
+          case Constants.ServiceStates.STARTING:
+            reject(new Error("LanguageTool called on starting service."));
+            break;
+          case Constants.ServiceStates.STOPPING:
+            reject(new Error("LanguageTool called on stopping service."));
+            break;
+          case Constants.ServiceStates.ERROR:
+            reject(new Error("LanguageTool called on errored service."));
+            break;
+          default:
+            this._state = Constants.ServiceStates.ERROR;
+            reject(new Error("LanguageTool called on unknown service state."));
+            break;
+        }
+      } else if (url === undefined) {
+        this._state = Constants.ServiceStates.ERROR;
+        reject(new Error("LanguageTool URL is not defined"));
       } else {
-        reject("LanguageTool URL is not defined");
+        this._state = Constants.ServiceStates.ERROR;
+        reject(new Error("Unknown error"));
       }
     });
   }
+
   public start(): Promise<boolean> {
+    this._state = Constants.ServiceStates.STARTING;
     return new Promise((resolve) => {
+      this._state = Constants.ServiceStates.RUNNING;
       resolve(true);
     });
   }
 
   public stop(): Promise<void> {
+    this._state = Constants.ServiceStates.STOPPING;
     return new Promise((resolve) => {
+      this._state = Constants.ServiceStates.STOPPED;
       resolve();
     });
   }
@@ -101,30 +141,53 @@ export abstract class AbstractService
   public ping(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       const url = this._ltUrl;
-      if (url) {
+      if (this._state === Constants.ServiceStates.RUNNING && url) {
         const options = {
           method: "HEAD",
           uri: url,
           resolveWithFullResponse: true,
         };
-        Fetch.default(url, options).then((response) => {
-          if (response.status === 200) {
-            Constants.EXTENSION_OUTPUT_CHANNEL.appendLine(
-              `Ping to ${url} succeeded.`,
-            );
-            resolve(true);
-          } else {
-            Constants.EXTENSION_OUTPUT_CHANNEL.appendLine(
-              `Ping to ${url} failed with status ${response.status}.`,
-            );
-            reject(false);
-          }
-        });
+        Fetch.default(url, options)
+          .then((response) => {
+            if (response.status === 200) {
+              resolve(true);
+            } else {
+              reject(
+                new Error(
+                  `Ping to ${url} failed with status ${response.status}.`,
+                ),
+              );
+            }
+          })
+          .catch((err) => {
+            this._state = Constants.ServiceStates.ERROR;
+            reject(err);
+          });
+      } else if (this._state !== Constants.ServiceStates.RUNNING) {
+        switch (this._state) {
+          case Constants.ServiceStates.STOPPED:
+            reject(new Error("Ping called on stopped service."));
+            break;
+          case Constants.ServiceStates.STARTING:
+            reject(new Error("Ping called on starting service."));
+            break;
+          case Constants.ServiceStates.STOPPING:
+            reject(new Error("Ping called on stopping service."));
+            break;
+          case Constants.ServiceStates.ERROR:
+            reject(new Error("Ping called on errored service."));
+            break;
+          default:
+            this._state = Constants.ServiceStates.ERROR;
+            reject(new Error("Ping called on unknown service state."));
+            break;
+        }
+      } else if (url === undefined) {
+        this._state = Constants.ServiceStates.ERROR;
+        reject(new Error("LanguageTool URL is not defined"));
       } else {
-        Constants.EXTENSION_OUTPUT_CHANNEL.appendLine(
-          "Ping called, but LanguageTool URL is not defined",
-        );
-        reject(false);
+        this._state = Constants.ServiceStates.ERROR;
+        reject(new Error("Unknown error"));
       }
     });
   }
