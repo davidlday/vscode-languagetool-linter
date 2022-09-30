825ce5da3f580cb0e0eee344585dc3d24f1cb353
David L. Day <dday376@gmail.com>
2022-09-30 15:12:27 -0400

    commented out PodmanMachine interface

--- src/services/PodmanService.ts
+++ src/services/PodmanService.ts
@@ -65,22 +65,22 @@ interface ContainerInfo {
   };
 }
 
-interface PodmanMachine {
-  Name: string;
-  Default: boolean;
-  Created: string;
-  Running: boolean;
-  Starting: boolean;
-  LastUp: string;
-  Stream: string;
-  VMType: string;
-  CPUs: number;
-  Memory: string;
-  DiskSize: string;
-  Port: number;
-  RemoteUsername: string;
-  IdentityPath: string;
-}
+// interface PodmanMachine {
+//   Name: string;
+//   Default: boolean;
+//   Created: string;
+//   Running: boolean;
+//   Starting: boolean;
+//   LastUp: string;
+//   Stream: string;
+//   VMType: string;
+//   CPUs: number;
+//   Memory: string;
+//   DiskSize: string;
+//   Port: number;
+//   RemoteUsername: string;
+//   IdentityPath: string;
+// }
 
 interface MachineInfo {
   Host: {
