bdbc3da3fdd534e17646ae2af286211a22b409af
David L. Day <dday376@gmail.com>
2026-04-11 22:22:55 -0400

    refactor: remove unnecessary break statement in FormattingProviderQuotes class

--- src/FormattingProviderQuotes.ts
+++ src/FormattingProviderQuotes.ts
@@ -81,7 +81,6 @@ export class FormattingProviderQuotes
               ),
             ];
           }
-          break;
         case FormattingProviderQuotes.singleQuote:
           if (
             [
